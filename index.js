8b5e53240bb8c67be374a1b53c157c1a6195bf6c
Your NameAhsanHabib002 <ahsanhabib12300@gmail.com>
2025-02-22 02:23:18 +0600

    JWT setup

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const cors = require('cors');
-require('dotenv').config()
+const cors = require("cors");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
 const port = process.env.PORT || 5000;
 
 // middleware
@@ -10,7 +11,7 @@ app.use(express.json());
 
 // mongodb
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.9fdmi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -19,23 +20,49 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-    
+
     const taskCollection = client.db("taskDB").collection("task");
     const usersCollection = client.db("taskDB").collection("users");
 
-    app.get('/task', async(req,res)=>{
-        const result =await taskCollection.find().toArray();
-        res.send(result);
+    // JWT API
+    app.post("/jwt", async (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "5h",
+      });
+      res.send({ token });
     });
 
-    // users
+    // middlewares
+    const verifyToken = (req, res, next) => {
+      if (!req.headers.authorization) {
+        return res.status(401).send({ message: "Unauthorization" });
+      }
+      const token = req.headers.authorization.split(" ")[1];
+      jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+          return res.status(401).send({ message: "Unauthorized" });
+        }
+        req.decoded = decoded;
+        next();
+      });
+    };
+    
+    // task API
+
+    app.get("/task", async (req, res) => {
+      const result = await taskCollection.find().toArray();
+      res.send(result);
+    });
+
+    // users API
     app.get("/users", async (req, res) => {
       const result = await usersCollection.find().toArray();
       res.send(result);
@@ -54,7 +81,9 @@ async function run() {
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -62,13 +91,10 @@ async function run() {
 }
 run().catch(console.dir);
 
-
-
-
-app.get('/',(req,res)=>{
-    res.send('Daily Task is Active')
-})
+app.get("/", (req, res) => {
+  res.send("Daily Task is Active");
+});
 
 app.listen(port, () => {
-    console.log(`Daily Task is Active  on port${port}`);
-})
+  console.log(`Daily Task is Active  on port${port}`);
+});
